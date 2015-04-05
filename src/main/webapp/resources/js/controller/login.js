fd99c5de25ad28467f114c4212db64e67aad0efe
gejun123456 <gejunxxx@163.com>
2015-04-06 02:11:04 +0800

    fix most thing

--- src/main/webapp/resources/js/controller/login.js
+++ src/main/webapp/resources/js/controller/login.js
@@ -2,16 +2,19 @@
  * Created by Administrator on 4/4/2015.
  */
 var loginA = angular.module('login', []);
-loginA.controller('loginController', function ($scope, $http) {
+loginA.controller('loginController', function ($scope, $http, $location) {
     $scope.login = function () {
         console.log("nimei");
         console.log($scope.userName);
         console.log($scope.password);
-        $http.post('/login', {"user_Id": $scope.userName, "password": $scope.password})
+        $http.post('login', {"user_Id": $scope.userName, "password": $scope.password})
             .success(function (data, status, headers, config) {
                 if (data.type == "success") {
-                    window.location.href = "/";
+                    console.log("hi");
+                    console.log(window.location.href);
+                    window.location.href="./";
                 } else {
+                    console.log("not right");
                     $scope.noexist = true;
                 }
             });
