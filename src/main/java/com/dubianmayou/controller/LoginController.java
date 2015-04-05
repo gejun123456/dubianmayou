8f0b854037cffbcd62d2dfbf71b47e44417830cb
gejun123456 <gejunxxx@163.com>
2015-04-05 09:05:24 +0800

    add ajax call

--- src/main/java/com/dubianmayou/controller/LoginController.java
+++ src/main/java/com/dubianmayou/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by bruce on 4/3/2015.
@@ -16,8 +17,9 @@ public class LoginController {
     @Autowired
     private LoginDao loginDao;
 
+
     @RequestMapping(method = RequestMethod.POST)
-    public String getIndexPage(@RequestParam String username, @RequestParam String password) {
+    public @ResponseBody String getIndexPage(@RequestParam String username, @RequestParam String password) {
         if (loginDao.login(username, password)) {
             return "loginss";
         } else {
--- src/main/webapp/resources/js/controller/login.js
+++ src/main/webapp/resources/js/controller/login.js
@@ -2,10 +2,14 @@
  * Created by Administrator on 4/4/2015.
  */
 var loginA = angular.module('login', []);
-loginA.controller('loginController', function ($scope) {
+loginA.controller('loginController', function ($scope, $http) {
     $scope.login = function () {
         console.log("nimei");
         console.log($scope.userName);
         console.log($scope.password);
+        $http.post('/login', {username: $scope.userName, password: $scope.password})
+            .success(function (data) {
+                console.log(data);
+            });
     }
 });
