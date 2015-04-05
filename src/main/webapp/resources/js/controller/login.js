968cc9eb161b6e1591f537378bacec6cdd001521
gejun123456 <gejunxxx@163.com>
2015-04-06 03:49:12 +0800

    fix all name could use with tomcat

--- src/main/webapp/resources/js/controller/login.js
+++ src/main/webapp/resources/js/controller/login.js
@@ -7,12 +7,12 @@ loginA.controller('loginController', function ($scope, $http, $location) {
         console.log("nimei");
         console.log($scope.userName);
         console.log($scope.password);
-        $http.post('login', {"user_Id": $scope.userName, "password": $scope.password})
+        $http.post('/login', {"user_Id": $scope.userName, "password": $scope.password})
             .success(function (data, status, headers, config) {
                 if (data.type == "success") {
                     console.log("hi");
                     console.log(window.location.href);
-                    window.location.href="./";
+                    window.location.href="/";
                 } else {
                     console.log("not right");
                     $scope.noexist = true;
