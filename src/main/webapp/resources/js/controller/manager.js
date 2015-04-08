a6d1cd7502c321ef31e40ba5f401822cf2a6a44a
gejun123456 <gejunxxx@163.com>
2015-04-08 19:35:36 +0800

    grid could display

--- src/main/webapp/resources/js/controller/manager.js
+++ src/main/webapp/resources/js/controller/manager.js
@@ -2,27 +2,5 @@
 //console.log(b);
 var mai = angular.module('managerUser', ['ui.grid', 'ui.grid.edit']);
 mai.controller('managerController', ['$scope', function ($scope) {
-    console.log("hi");
-    $scope.name = "nimei";
-    console.log(bb);
     $scope.myData = bb;
-    $scope.gridOptions = {
-        enableSorting: true,
-        columnDefs: [
-            {name: 'firstName', field: 'first-name'},
-            {name: '1stFriend', field: 'friends[0]'},
-            {name: 'city', field: 'address.city'},
-            {name: 'getZip', field: 'getZip()', enableCellEdit: false}
-        ],
-        data: [{
-            "first-name": "Cox",
-            "friends": ["friend0"],
-            "address": {street: "301 Dove Ave", city: "Laurel", zip: "39565"},
-            "getZip": function () {
-                return this.address.zip;
-            }
-        }
-        ]
-    };
-
 }]);
