486ffe292c7e95df3874178e74a331a7fddfd34a
gejun123456 <gejunxxx@163.com>
2015-04-20 20:10:41 +0800

    add countAll

--- src/main/java/com/dubianmayou/controller/VoteController.java
+++ src/main/java/com/dubianmayou/controller/VoteController.java
@@ -1,12 +1,15 @@
 package com.dubianmayou.controller;
 
 import com.dubianmayou.entity.VoteResult;
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by bruce on 4/20/2015.
@@ -17,21 +20,41 @@ public class VoteController {
     @RequestMapping(value = "/count/{name}", method = RequestMethod.GET)
     @ResponseBody
     public String getCount(@PathVariable String name) {
-        if (name.equals("mayu")) {
-            VoteResult result = new VoteResult();
-            result.setName("mayu");
-            result.setCount(4);
-            return new Gson().toJson(result);
-        } else if (name.equals("zhiyuan")) {
-            VoteResult result = new VoteResult();
-            result.setName("zhiyuan");
-            result.setCount(2);
-            return new Gson().toJson(result);
-        } else {
-            VoteResult result = new VoteResult();
-            result.setName("me");
-            result.setCount(1);
-            return new Gson().toJson(result);
-        }
+//        if (name.equals("mayu")) {
+//            VoteResult result = new VoteResult();
+//            result.setName("mayu");
+//            result.setCount(4);
+//            return new Gson().toJson(result);
+//        } else if (name.equals("zhiyuan")) {
+//            VoteResult result = new VoteResult();
+//            result.setName("zhiyuan");
+//            result.setCount(2);
+//            return new Gson().toJson(result);
+//        } else {
+//            VoteResult result = new VoteResult();
+//            result.setName("me");
+//            result.setCount(1);
+//            return new Gson().toJson(result);
+//        }
+        return "";
+    }
+
+    @RequestMapping(value = "/countAll", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllCount() {
+        List<VoteResult> votes = Lists.newArrayList();
+        VoteResult ra = new VoteResult();
+        ra.setName("mayu");
+        ra.setCount(4);
+        votes.add(ra);
+        VoteResult rb = new VoteResult();
+        rb.setName("zhiyuan");
+        rb.setCount(2);
+        VoteResult rc = new VoteResult();
+        rc.setName("me");
+        rc.setCount(1);
+        votes.add(rb);
+        votes.add(rc);
+        return new Gson().toJson(votes);
     }
 }
--- src/main/webapp/resources/js/controller/indexco.js
+++ src/main/webapp/resources/js/controller/indexco.js
@@ -2,21 +2,18 @@
  * Created by Administrator on 4/8/2015.
  */
 $(document).ready(function () {
-    var allmember = ['mayu', 'zhiyuan', 'me'];
-    for (var i = 0; i < allmember.length; i++) {
-        var akbid = allmember[i];
-        console.log(akbid);
-        var url = "vote/count/" + akbid;
-        $.get(url, function (data, status) {
-            var c = jQuery.parseJSON(data);
-            var h = "#"+ c.name;
-            $(h).html(c.count);
-        });
-    }
-    //var c = "#mayu";
-    //// jQuery methods go here...
-    //$("#addmayu").click(function () {
-    //    a = a + 1;
-    //    $(c).html(a);
-    //})
+    $.get("vote/countAll", function (data, status) {
+        var c = jQuery.parseJSON(data);
+        console.log(c);
+        for(var i =0;i< c.length;i++){
+            var akbid = c[i].name;
+            $("#"+akbid).html(c[i].count);
+        }
+    });
+//var c = "#mayu";
+//// jQuery methods go here...
+//$("#addmayu").click(function () {
+//    a = a + 1;
+//    $(c).html(a);
+//})
 });
