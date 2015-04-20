8c708ce8e3ca6ac72b5ca454b58b2d398f27555d
gejun123456 <gejunxxx@163.com>
2015-04-20 18:51:39 +0800

    add voteController

---
+++ src/main/java/com/dubianmayou/controller/VoteController.java
@@ -0,0 +1,13 @@
+package com.dubianmayou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by bruce on 4/20/2015.
+ */
+@Controller
+@RequestMapping("/vote")
+public class VoteController {
+
+}
--- src/main/webapp/resources/js/controller/indexco.js
+++ src/main/webapp/resources/js/controller/indexco.js
@@ -2,11 +2,17 @@
  * Created by Administrator on 4/8/2015.
  */
 $(document).ready(function () {
+    var allmember = ['mayu','zhiyuan','me'];
+    for(var i =0;i<allmember.length;i++){
+        console.log(allmember[i]);
+
+    }
     var a = 0;
-    $("#countmayu").html(a);
+    var c ="#mayu";
+    $(c).html(a);
     // jQuery methods go here...
     $("#addmayu").click(function () {
         a = a+1;
-        $("#countmayu").html(a);
+        $(c).html(a);
     })
 });
