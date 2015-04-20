039cc2192a5918613be082fee0d64c3331e07044
gejun123456 <gejunxxx@163.com>
2015-04-20 18:39:21 +0800

    add vote

--- src/main/webapp/resources/js/controller/indexco.js
+++ src/main/webapp/resources/js/controller/indexco.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 4/8/2015.
  */
+$(document).ready(function () {
+    var a = 0;
+    $("#countmayu").html(a);
+    // jQuery methods go here...
+    $("#addmayu").click(function () {
+        a = a+1;
+        $("#countmayu").html(a);
+    })
+});
