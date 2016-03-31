2e5d61d77a7cb1bb31436d01b6c02222aa3550d8
gejun <bruce.ge@dianping.com>
2016-03-31 22:59:04 +0800

    add with test

--- src/main/java/com/dubianmayou/dao/mapper/UserMapper.java
+++ src/main/java/com/dubianmayou/dao/mapper/UserMapper.java
@@ -1,11 +1,12 @@
 package com.dubianmayou.dao.mapper;
 
+import com.dubianmayou.dao.slave.mapper.SlaveUserMapper;
 import com.dubianmayou.entity.User;
 import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by bruce on 4/4/2015.
  */
-public interface UserMapper {
-    User loadUser(@Param("userName") String userName, @Param("password") String password);
+public interface UserMapper extends SlaveUserMapper {
+    User loadUser2(@Param("userName") String userName, @Param("password") String password);
 }
