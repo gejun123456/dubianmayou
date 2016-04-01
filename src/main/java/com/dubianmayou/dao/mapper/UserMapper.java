04c30c0992b48395449fb52127150615b5ca8162
gejun <bruce.ge@dianping.com>
2016-04-01 22:46:32 +0800

    fix mapper

--- src/main/java/com/dubianmayou/dao/mapper/UserMapper.java
+++ src/main/java/com/dubianmayou/dao/mapper/UserMapper.java
@@ -1,12 +1,13 @@
 package com.dubianmayou.dao.mapper;
 
-import com.dubianmayou.dao.slave.mapper.SlaveUserMapper;
 import com.dubianmayou.entity.User;
 import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by bruce on 4/4/2015.
  */
-public interface UserMapper extends SlaveUserMapper {
+public interface UserMapper {
     User loadUser2(@Param("userName") String userName, @Param("password") String password);
+
+    User loadUser(@Param("userName") String userName, @Param("password") String password);
 }
--- src/main/java/com/dubianmayou/dao/slave/mapper/SlaveUserMapper.java
+++ src/main/java/com/dubianmayou/dao/slave/mapper/SlaveUserMapper.java
@@ -1,11 +1,9 @@
 package com.dubianmayou.dao.slave.mapper;
 
-import com.dubianmayou.entity.User;
-import org.apache.ibatis.annotations.Param;
+import com.dubianmayou.dao.mapper.UserMapper;
 
 /**
  * Created by bruce on 4/4/2015.
  */
-public interface SlaveUserMapper {
-    User loadUser(@Param("userName") String userName, @Param("password") String password);
+public interface SlaveUserMapper extends UserMapper {
 }
