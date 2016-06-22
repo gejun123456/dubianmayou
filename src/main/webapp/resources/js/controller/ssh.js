3783eb357ee4dac79afbc9b40719066d8768e5ce
gejun <bruce.ge@dianping.com>
2016-06-22 13:02:58 +0800

    fix add web page

--- src/main/webapp/resources/js/controller/ssh.js
+++ src/main/webapp/resources/js/controller/ssh.js
@@ -5,11 +5,16 @@ $(document).ready(function(){
     $(".myapp").click(function (event) {
         // alert(event.target.id)
         var id = event.target.id
+        var html="";
         $.get("/ssh/"+id, function(data){
-            alert("Data Loaded: " + data);
+            var device = $.parseJSON(data);
+            for(key in device){
+                html = html+device[key].hostname+" "+device[key].private_ip+"<br>";
+            }
+            var changid = "div"+id;
+            //send post data choose the way to use.
+            $("#"+changid).html(html)
         });
-        var changid = "div"+id;
-        //send post data choose the way to use.
-        // $("#"+changid).html("nimeiya")
+
     })
 });
